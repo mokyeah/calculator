70020d109cb7a6029c160a3d5f5b18785c880e22
mokyeah <mzmmzm2008@gmail.com>
2022-08-13 15:02:41 +0800

    d

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,32 @@ function App() {
         <div className="bg-white p-5 relative">
           <div className="flex flex-row gap-3 ">
             <div className="text-s ">10:60</div>
+            <div className="text-s">calculator</div>
             <Icon icon="ant-design:signal-filled" className="mt-1" />
-            <Icon icon="akar-icons:wifi" className="mt-1 "/>
-            
-            </div>
-            <div className='flex flex-row items-center gap-4 border p-5 border-neutral-700 mt-5 animate__animated animate__bounceInLeft'></div>
+            <Icon icon="akar-icons:wifi" className="mt-1 " />
+            <Icon icon="carbon:battery-full" className="mt-1"></Icon>
 
           </div>
-          <div className="">hello</div>
-          <div classname="text-md font-seimifold"></div>
+          <div className='flex flex-row items-center gap-4 border p-5 border-neutral-700 mt-5 animate__animated animate__bounceInLeft'>
+            the calculator is good
+          </div>
           <div>
-            <span classname="font-semifold"></span>
-            <div classname="text-md font-seimifold text-xl mt-4 tracking-wider" >
-              <strong ></strong></div>
-              </div>
-              </div>
-
+            <div className="text-3xl p-4 gap-3 font-bold md:p-2 text-xl gap-3 font-bold" >7</div>
+            <div className="text-3xl p-4 gap-3 font-bold md:p-2 text-xl gap-3 font-bold" >8</div>
+            <div className="text-3xl p-4 gap-3 font-bold md:p-2 text-xl gap-3 font-bold" >9</div>
+          </div>
         </div>
-        
+
+        <div classname="text-md font-seimifold"></div>
+        <div>
+          <span classname="font-semifold"></span>
+          <div classname="texyt-md font-seimifold text-xl mt-4 tracking-wider" >
+            <strong ></strong></div>
+        </div>
+      </div>
+
+    </div>
+
   )
 }
 
